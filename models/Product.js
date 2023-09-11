ca3e933027aed6e249fe7c3cd2fc72df3cba14a2
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-09-11 14:57:26 -0700

    fixes build issue with Product model

--- models/Product.js
+++ models/Product.js
@@ -10,10 +10,10 @@ class Product extends Model {}
 Product.init(
   {
     id: {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
+      autoIncrement: true,
     },
     product_name:{
       type: DataTypes.STRING,
