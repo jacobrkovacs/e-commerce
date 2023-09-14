db75f4fe1e1b7d0631217e3dbc27c7885e358395
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-09-14 14:20:28 -0700

    fixed bug for DELETE

--- models/index.js
+++ models/index.js
@@ -7,22 +7,25 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  onDelete: 'CASCADE'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  onDelete: 'CASCADE'
 });
 
 module.exports = {
